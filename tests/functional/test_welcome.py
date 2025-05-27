b2b77ef1eeb80b9e95bb52faa3cdccda6f4254f6
Steven Fawcett <steve.fawcett@capgemini.com>
2025-05-27 14:15:19 +0100

    Intital Cut

---
+++ tests/functional/test_welcome.py
@@ -0,0 +1,16 @@
+from manager import create_app
+
+def test_home_page():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (GET)
+    THEN check that the response is valid
+    """
+    # Set the Testing configuration prior to creating the Flask application
+    flask_app = create_app()
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/')
+        assert response.status_code == 200
+        assert b"Hello, World!" in response.data
